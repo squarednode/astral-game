c0727e0f77b24ef8cb8a41de353be78469ab853a
Christopher Erickson <80534741+squarednode@users.noreply.github.com>
2026-08-13 12:44:13 -0400

    0.6.9-2.5d.3m consolidate polish2 entry modules

---
+++ src/prototypes/sideview/polish2Entry.ts
@@ -0,0 +1,7 @@
+import './main3a';
+import './cameraTuning3f';
+import './forestTheme3k';
+import './forestPolish3l';
+import './cameraPolish3g';
+import './forestEdgeFix3m';
+import './cameraReturnFix3h';
